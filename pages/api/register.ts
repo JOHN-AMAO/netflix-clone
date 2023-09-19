388e516bd7e33b01def8faddd83cc4124f92df7f
JOHN-AMAO <amaojohn767@gmail.com>
2023-09-19 20:21:12 +0100

    "19th september"

---
+++ pages/api/register.ts
@@ -0,0 +1,12 @@
+import bcrypt from "bcrypt";
+import { NextApiRequest, NextApiResponse } from "next";
+import prismadb from "@/lib/prismadb";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method !== "POST") {
+    return res.status(405).end();
+  }
+}
